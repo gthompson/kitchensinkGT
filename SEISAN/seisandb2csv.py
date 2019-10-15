4c9f71b87e7b05bcbba30905da6ef9bf18905398
Glenn Thompson <thompsong@mail.usf.edu>
2019-10-16 00:21:02 +0200

    changed comments on usage

--- SEISAN/seisandb2csv.py
+++ SEISAN/seisandb2csv.py
@@ -1,4 +1,15 @@
 #!/Users/thompsong/miniconda3/bin/python
+# /Users/thompsong/src/volcanoObsPy/SEISAN/seisandb2csv.py
+# Traverses a Seisan database structure by REA/year/month finding all the S-files and
+# corresponding WAV files, and generates a summary line for each trace.
+# Will generate nothing for an Sfile for which no corresponding WAVfile found
+# Output files are like DBNAMEcatalogYYYYMM.csv
+# These should be readable into Python dataframes
+# They can also easily be manipulated with grep and awk, e.g.
+# 1. Only print 4th column:
+#    awk -F',' '{print $4}' MVOE_catalog*.csv
+# 2. Only show 5th column for station MBWH
+#    grep MWBH MVOE_catalog*.csv | awk -F',' '{print $5}'
 import os, sys, glob, obspy.core
 #sys.path.insert(1, '/Users/thompsong/src/volcanoObsPy')
 sys.path.insert(1, '/Users/thompsong/src/volcanoObsPy/SEISAN')
@@ -31,11 +42,8 @@ def generate_monthly_csv(mm):
                          tracenum, tr.id, thissfile ))
                      tracenum += 1
     fptr.close()
-# Run this with bin/seisandb2csv
-# awk '{print $4}' catalog.txt
 
-#sfileslist = sorted(glob.glob('./seismo/REA/MVOE_/2005/??/*'))
-#sfileslist = sorted(glob.glob('./seismo/REA/MVOE_/2005/??/*'))
+
 print("Type the path to your Seisan directory [default: ./seismo]")
 SEISAN_TOP = input(" ? ")
 if not SEISAN_TOP:
