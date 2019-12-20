9c029ee6f441a3ef0545a184f00c7be9d953b9ee
Glenn Thompson <thompsong@mail.usf.edu>
2019-12-19 23:32:36 -0500

    moved seisan codes from Alexis_montserrat_codes

--- SEISAN/seisanwavdb2csv.py
+++ SEISAN/seisanwavdb2csv.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-# Traverses a Seisan database structure by REA/year/month finding all the S-files and
-# corresponding WAV files, and generates a summary line for each trace.
+# Traverses a Seisan database structure by WAV/db/year/month finding all the WAV-files and generate a summary line for each trace
 #
-# Will generate nothing for an Sfile for which no corresponding WAVfile found
-# Output files are like DBNAMEcatalogYYYYMM.csv
+# Output files are like DBNAMEwavfilesYYYYMM.csv
 #
 # These will later be read into Python dataframes
 import sys, glob, os
@@ -12,8 +10,8 @@ import Seisan_Catalog
 SEISAN_DATA = os.environ["SEISAN_DATA"]
 if not SEISAN_DATA:
     SEISAN_DATA = "./seismo"
-print(sys.argv)
-print(len(sys.argv))
+#print(sys.argv)
+#print(len(sys.argv))
 if len(sys.argv)>1:
     SEISAN_DB = sys.argv[1]
 else:
@@ -26,13 +24,11 @@ else:
     if not SEISAN_DB:
         SEISAN_DB = "MVOE_"
 
-yyyylist = sorted(glob.glob(os.path.join(SEISAN_DATA, 'REA', SEISAN_DB, '????')))
+yyyylist = sorted(glob.glob(os.path.join(SEISAN_DATA, 'WAV', SEISAN_DB, '????')))
 print(yyyylist)
-flag_sfiles_only = True
-#flag_sfiles_only = False
 for yyyy in yyyylist:
     mmlist = sorted(glob.glob(os.path.join(yyyy, '??')))
     for mm in mmlist:
         #print("Processing %s:" % mm)
-        Seisan_Catalog.generate_monthly_csv(mm, flag_sfiles_only)
+        Seisan_Catalog.generate_monthly_wav_csv(mm)
 
