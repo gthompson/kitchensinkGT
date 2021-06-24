2e842c02dd0fda3ef638a823156bfa17b1e2eb38
Glenn Thompson <glennthompson1971@gmail.com>
2021-06-24 10:42:29 -0400

    Major reorganization

--- CONVERT/miniseed2sac.py
+++
@@ -1,7 +0,0 @@
-from obspy.core import *
-import sys
-for file in sys.argv[1:]:
-    st = read(file)
-    for tr in st:
-    outfile = "%s.%s.%s" % (tr.stats.station, tr.stats.channel, tr.stats.starttime)
-    tr.write("outfile + ".sac", format="SAC")
