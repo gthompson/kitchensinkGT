c0cecaad40771291fbc14b7f5729b915537daccb
Glenn Thompson <thompsong@mail.usf.edu>
2021-08-24 09:16:59 -0400

    added some exception handling that probably wasn't needed

--- PROJECTS/MontserratML/00_convert_seisandb_to_csvfiles.py
+++ PROJECTS/MontserratML/00_convert_seisandb_to_csvfiles.py
@@ -87,6 +87,8 @@ def WAV2picklefile(paths):
             if bool_correct_data: # try to find corresponding station XML
                 this_inv = load_mvo_inventory(tr, paths['CALDIR'])
             process_trace(tr, inv=this_inv)
+            if len(tr.data)<1000:
+                tr.stats.quality_factor = -1.0
         
         # remove bad traces
         for tr in st:    
@@ -299,6 +301,9 @@ def processWAV(wavfile):
     wavrow={}
     
     st = WAV2picklefile(paths)
+    for tr in st:
+        if len(tr.data)<1000:
+            st.remove(tr) 
     st.select(component='[ENZ]') # subset to seismic components only
     if len(st)==0:
         return wavrow
@@ -411,5 +416,7 @@ bool_detect_event=True
 bool_overwrite=False
 startdate = dt.datetime(1995,7,18,0,0,0)
 startdate = dt.datetime(2000,3,1,0,0,0)
+startdate = dt.datetime(2002,9,1,0,0,0)
+startdate = dt.datetime(2003,4,2,3,0,0)
 # 2000-02-29-2338-36S.MVO___019
 main(SEISAN_DB, 999999, startdate)
